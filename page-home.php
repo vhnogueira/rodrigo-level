c5a62e7c447a526ed350bd1c05963878d2921981
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-08 02:11:41 -0300

    First commit

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+Home
+
+<?php get_footer();
