e7fec62641b8b203ede1695b1f938bdaeb6a99b6
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-08 02:45:20 -0300

    Added theme name

--- header.php
+++ header.php
@@ -21,7 +21,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<body <?php body_class(); ?>
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
