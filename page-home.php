620e0304f5df4e21cdee0fc0e356ff0837f7898a
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-08 02:41:01 -0300

    first row home

--- page-home.php
+++ page-home.php
@@ -11,6 +11,17 @@ defined( 'ABSPATH' ) || exit;
 
 get_header(); ?>
 
-Home
+<section id="home-1" class="py-5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+            
+            </div>
+            <div class="col-md-8 bg-dark">
+            
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer();
